6cbbad055f1735b2fba66802a91969b5eb010322
yunko006 <peyrachethomas@gmail.com>
2021-10-21 12:30:09 +0200

    Solution for Square Every Digit kata

---
+++ 7-kyu/SquareEveryDigit.py
@@ -0,0 +1,12 @@
+def square_digits(num):
+    final_num = ""
+
+    
+    for n in str(num):
+        x = int(n)**2
+        final_num += f"{x}"
+    return int(final_num)
+
+
+a = square_digits(912)
+print(a)
