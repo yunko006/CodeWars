7c4724c80cb4b066be1fb397d40ff2c76038ca76
yunko006 <peyrachethomas@gmail.com>
2021-10-25 23:46:55 +0200

    Backspaces in string kata solution

---
+++ 6-kyu/BackspacesInString.py
@@ -0,0 +1,20 @@
+"""
+"abc#d##c"      ==>  "ac"
+"abc##d######"  ==>  ""
+"#######"       ==>  ""
+""              ==>  ""
+"""
+
+
+def clean_string(s):
+    cleaned = []
+
+    for char in s:
+        if char != '#':
+            cleaned.append(char)
+        else:
+            if cleaned:  # if list is not empty
+                # dont use remove or it will delete all same char from the list
+                cleaned.pop()
+
+    return ''.join(cleaned)
